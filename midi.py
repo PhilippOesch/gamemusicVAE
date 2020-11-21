0fec60596383ae02e2e4616af4376704101b2f4d
Philipp Oeschger <philipp.oeschger@hs-furtwangen.de>
2020-11-21 16:53:48 +0100

    setup

--- midi.py
+++ midi.py
@@ -81,16 +81,16 @@ def samples_to_midi(samples, fname, ticks_per_sample, thresh=0.5):
 	abs_time = 0
 	last_time = 0
 	for sample in samples:
-		for y in xrange(sample.shape[0]):
+		for y in range(sample.shape[0]):
 			abs_time += ticks_per_sample
-			for x in xrange(sample.shape[1]):
+			for x in range(sample.shape[1]):
 				note = x + (128 - num_notes)/2
 				if sample[y,x] >= thresh and (y == 0 or sample[y-1,x] < thresh):
 					delta_time = abs_time - last_time
-					track.append(Message('note_on', note=note, velocity=127, time=delta_time))
+					track.append(Message('note_on', note=int(note), velocity=127, time=int(delta_time)))
 					last_time = abs_time
 				if sample[y,x] >= thresh and (y == sample.shape[0]-1 or sample[y+1,x] < thresh):
 					delta_time = abs_time - last_time
-					track.append(Message('note_off', note=note, velocity=127, time=delta_time))
+					track.append(Message('note_off', note=int(note), velocity=127, time=int(delta_time)))
 					last_time = abs_time
 	mid.save(fname)
--- util.py
+++ util.py
@@ -48,16 +48,16 @@ def sample_to_pic(fname, sample, thresh=None):
 
 def samples_to_pics(dir, samples, thresh=None):
 	if not os.path.exists(dir): os.makedirs(dir)
-	for i in xrange(samples.shape[0]):
+	for i in range(samples.shape[0]):
 		sample_to_pic(dir + '/s' + str(i) + '.png', samples[i], thresh)
 
 def pad_songs(y, y_lens, max_len):
 	y_shape = (y_lens.shape[0], max_len) + y.shape[1:]
 	y_train = np.zeros(y_shape, dtype=np.float32)
 	cur_ix = 0
-	for i in xrange(y_lens.shape[0]):
+	for i in range(y_lens.shape[0]):
 		end_ix = cur_ix + y_lens[i]
-		for j in xrange(max_len):
+		for j in range(max_len):
 			k = j % (end_ix - cur_ix)
 			y_train[i,j] = y[cur_ix + k]
 		cur_ix = end_ix
@@ -83,8 +83,8 @@ def embed_samples(samples):
 	n, m, p = samples.shape
 	samples.flags.writeable = False
 	e_samples = np.empty(samples.shape[:2], dtype=np.int32)
-	for i in xrange(n):
-		for j in xrange(m):
+	for i in range(n):
+		for j in range(m):
 			note = samples[i,j].data
 			if note not in note_dict:
 				note_dict[note] = len(note_dict)
@@ -98,8 +98,8 @@ def embed_samples(samples):
 def e_to_samples(e_samples, lookup):
 	samples = np.empty(e_samples.shape + lookup.shape[-1:], dtype=np.float32)
 	n, m = e_samples.shape
-	for i in xrange(n):
-		for j in xrange(m):
+	for i in range(n):
+		for j in range(m):
 			samples[i,j] = lookup[e_samples[i,j]]
 	return samples
 	
