13f5ad7d7191af9ce0a1aa62dc5b44a9219444b3
PhilippOesch <49947694+PhilippOesch@users.noreply.github.com>
2021-01-20 20:49:49 +0100

    PP, EB, UPC Metrics implementet + HCDF

--- src/metrics.py
+++ src/metrics.py
@@ -5,18 +5,64 @@ num_notes = 88
 
 warnings.filterwarnings("ignore",category =RuntimeWarning)
 
-def tonal_distance(samples, ignore_treshhold= True, thresh=0.5):
+# def tonal_distance(samples, ignore_treshhold= True, thresh=0.5):
 
-    # tranform to chroma vector of size 12
-    # 12 Tone Equal Temperament
-    eq_t_samples = np.zeros(
-        (samples.shape[0], samples.shape[1], 12), dtype=np.float32)
+#     # tranform to chroma vector of size 12
+#     # 12 Tone Equal Temperament
+#     eq_t_samples = samples_to_pitchclasses(samples)
 
-    # 6d Vectors for tonal centroid calculation
+#     # 6d Vectors for tonal centroid calculation
+#     t_c_samples = np.zeros(
+#         (samples.shape[0], samples.shape[1], 6), dtype=np.float32)
+
+#     # print(eq_t_samples)
+
+#     for z in range(eq_t_samples.shape[0]):
+#         for y in range(eq_t_samples.shape[1]):
+#             y_d6= [get_transformation_matrix(x)* eq_t_samples[z ,y, x] for x in range(eq_t_samples.shape[2])]
+#             sum_y= np.sum(y_d6, axis= 0);
+#             # print(sum_y)
+#             l_norm= np.linalg.norm(eq_t_samples[z, y])
+#             zeta_y= sum_y * (1/ l_norm)
+#             zeta_y= np.nan_to_num(zeta_y)
+#             t_c_samples[z, y] = zeta_y
+
+# Harmonic change detection function
+def hcdf(samples, ignore_treshhold= True, thresh=0.5):
+#     # 12 Tone Equal Temperament
+    eq_t_samples = samples_to_pitchclasses(samples)
+
+#     # 6d Vectors for tonal centroid calculation
     t_c_samples = np.zeros(
         (samples.shape[0], samples.shape[1], 6), dtype=np.float32)
 
-    # print(samples.shape)
+#     # print(eq_t_samples)
+
+    for z in range(eq_t_samples.shape[0]):
+        for y in range(eq_t_samples.shape[1]):
+            y_d6= [get_transformation_matrix(x)* eq_t_samples[z ,y, x] for x in range(eq_t_samples.shape[2])]
+            sum_y= np.sum(y_d6, axis= 0);
+            # print(sum_y)
+            l_norm= np.linalg.norm(eq_t_samples[z, y])
+            zeta_y= sum_y * (1/ l_norm)
+            zeta_y= np.nan_to_num(zeta_y)
+            t_c_samples[z, y] = zeta_y
+    
+    t_c_samples= np.reshape(t_c_samples, ((t_c_samples.shape[0]* t_c_samples.shape[1]), t_c_samples.shape[2]))
+    print(t_c_samples.shape)
+
+    hcdf_results= []
+    for i in range(1, t_c_samples.shape[0]-1):
+        epsilon= np.sqrt(np.sum((t_c_samples[i-1]- t_c_samples[i+1])**2))
+        hcdf_results.append(epsilon)
+
+    return hcdf_results
+
+
+def samples_to_pitchclasses(samples, ignore_treshhold=True, thresh= 0.5):
+    eq_t_samples = np.zeros(
+        (samples.shape[0], samples.shape[1], 12), dtype=np.float32)
+
     for z in range(samples.shape[0]):
         for y in range(samples.shape[1]):
             note = np.zeros((12), dtype=np.float32)
@@ -28,21 +74,63 @@ def tonal_distance(samples, ignore_treshhold= True, thresh=0.5):
                 elif not ignore_treshhold and samples[z, y, x]> thresh:
                     note[i]= 1
             eq_t_samples[z, y] = note
+    
+    return eq_t_samples
 
-    # print(eq_t_samples)
+def empty_bars(samples, thresh= 0.05):
+    empty_bar_count= 0
+    for z in range(samples.shape[0]):
+        is_empty= True
+        for y in range(samples.shape[1]):
+            for x in range(samples.shape[2]):
+                if samples[z, y, x]>= thresh:
+                    is_empty= False;
+        if is_empty:
+            empty_bar_count+= 1;
+    
+    return empty_bar_count, samples.shape[0]
 
+# Number of used pitch classes (12 tonen equal temperament)
+def get_upc(samples, thresh= 0.25):
+    eq_t_samples= samples_to_pitchclasses(samples= samples, ignore_treshhold=False, thresh=thresh)
+
+    upcs= []
     for z in range(eq_t_samples.shape[0]):
+        pitch_classes= np.zeros((12), dtype= np.uint8) 
         for y in range(eq_t_samples.shape[1]):
-            y_d6= [get_transformation_matrix(x)* eq_t_samples[z ,y, x] for x in range(eq_t_samples.shape[2])]
-            sum_y= np.sum(y_d6, axis= 0);
-            # print(sum_y)
-            l_norm= np.linalg.norm(eq_t_samples[z, y])
-            zeta_y= sum_y * (1/ l_norm)
-            zeta_y= np.nan_to_num(zeta_y)
-            t_c_samples[z, y] = zeta_y
+            for x in range(eq_t_samples.shape[2]):
+                if pitch_classes[x]< eq_t_samples[z, y, x]:
+                    pitch_classes[x]= int(eq_t_samples[z, y, x])
 
-    print(t_c_samples)
+        upcs.append(np.sum(pitch_classes))
+    average_upcs= np.average(upcs)
+    
+    return upcs, average_upcs
 
+def polyphonicity(samples, relative_to_notes= True, thresh= 0.25):
+    adjusted_samples= np.zeros((samples.shape[0], samples.shape[1], samples.shape[2]), dtype= np.uint8)
+    for z in range(samples.shape[0]):
+        for y in range(samples.shape[1]):
+            for x in range(samples.shape[2]):
+                if samples[z, y, x]> thresh:
+                    adjusted_samples[z, y, x]= 1
+    
+    count_frames_note= 0
+    count_frames_multiple_notes= 0
+
+
+    for z in range(adjusted_samples.shape[0]):
+        for y in range(adjusted_samples.shape[1]):
+            if np.sum(adjusted_samples[z, y])>0:
+                count_frames_note+= 1
+            if np.sum(adjusted_samples[z, y])>1:
+                count_frames_multiple_notes+= 1
+
+    if relative_to_notes:   
+        ratio= count_frames_multiple_notes/ count_frames_note
+    else:
+        ratio= count_frames_multiple_notes/ (samples.shape[0]* samples.shape[1])
+    return ratio
 
 def get_transformation_matrix(l):
     r1 = 1
--- src/test.py
+++ src/test.py
@@ -1,8 +1,29 @@
 import os
 import util
+import numpy as np
+import matplotlib.pyplot as plt
 
 import params
 import musicVAE
 import metrics
 
-metrics.tonal_distance(musicVAE.y_train[0])
+samples= musicVAE.y_train[50]
+hcdf_results= metrics.hcdf(samples= samples)
+
+#plot hcdf_results
+plt.plot(hcdf_results)
+plt.ylabel('TCDF')
+plt.xlabel('Time step')
+plt.show()
+
+upcs, average_upcs= metrics.get_upc(samples= samples)
+pp= metrics.polyphonicity(samples, relative_to_notes=False)
+
+# test_set= np.zeros(
+#         (16, 96, 88), dtype=np.float32)
+
+empty_bars, bars= metrics.empty_bars(samples)
+print(upcs)
+print("Average used pitch classes: ", average_upcs)
+print( empty_bars, " of ", bars, " bars empty")
+print("Poliphonicity:", pp)
