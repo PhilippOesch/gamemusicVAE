9e764b0d945c7177e2aedc5369b892c24c631422
PhilippOesch <49947694+PhilippOesch@users.noreply.github.com>
2021-02-27 15:12:50 +0100

    update

--- src/evaluation_part_2.py
+++ src/evaluation_part_2.py
@@ -86,32 +86,32 @@ lowest_td_result2= 1
 
 
 # Evaluation Train Set
-# print("evaluating Train_Set1:")
-# train_set_model1 = np.load(val_dir1 + '/train_set_samples.npy')
-# random_idxs = np.random.choice(train_set_model1.shape[0], 100)
-# train_set_model1_reduced= [ train_set_model1[random_idxs[i]] for i in range(random_idxs.shape[0])]
-# train_set_model1_reduced= np.array(train_set_model1_reduced)
-# print("Test_Set Shape: ", train_set_model1.shape)
-# metrics.write_td_evaluation("battle_train_set_td.txt" ,model1name, "Battle Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= train_set_model1_reduced, thresh= thresh))
+print("evaluating Train_Set1:")
+train_set_model1 = np.load(val_dir1 + '/train_set_samples.npy')
+random_idxs = np.random.choice(train_set_model1.shape[0], 100)
+train_set_model1_reduced= [ train_set_model1[random_idxs[i]] for i in range(random_idxs.shape[0])]
+train_set_model1_reduced= np.array(train_set_model1_reduced)
+print("Test_Set Shape: ", train_set_model1.shape)
+metrics.write_td_evaluation("battle_train_set_td.txt" ,model1name, "Battle Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= train_set_model1_reduced, thresh= thresh))
 # Evaluation AI-Result-set
-print("evaluating AI_Results1")
-val_set_model1 = np.load(val_dir1 + '/battle_final_1_samples.npy')
-print("AI_Results Shape: ", val_set_model1.shape)
-metrics.write_td_evaluation("battle_sample_set_td.txt" ,model1name, "Battle Sample Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= val_set_model1, thresh= thresh))
+# print("evaluating AI_Results1")
+# val_set_model1 = np.load(val_dir1 + '/battle_final_1_samples.npy')
+# print("AI_Results Shape: ", val_set_model1.shape)
+# metrics.write_td_evaluation("battle_sample_set_td.txt" ,model1name, "Battle Sample Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= val_set_model1, thresh= thresh))
 
 
-# Evaluation Train Set
-print("evaluating Train_Set2:")
-train_set_model2 = np.load(val_dir2 + '/train_set_samples.npy')
-random_idxs = np.random.choice(train_set_model2.shape[0], 100)
-train_set_model2_reduced= [ train_set_model2[random_idxs[i]] for i in range(random_idxs.shape[0])]
-train_set_model2_reduced= np.array(train_set_model2_reduced)
-print("Test_Set Shape: ", train_set_model2.shape)
-metrics.write_td_evaluation("overworld_train_set_td.txt" ,model1name, "Overworld Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= train_set_model2_reduced, thresh= thresh))
+# # Evaluation Train Set
+# print("evaluating Train_Set2:")
+# train_set_model2 = np.load(val_dir2 + '/train_set_samples.npy')
+# random_idxs = np.random.choice(train_set_model2.shape[0], 100)
+# train_set_model2_reduced= [ train_set_model2[random_idxs[i]] for i in range(random_idxs.shape[0])]
+# train_set_model2_reduced= np.array(train_set_model2_reduced)
+# print("Test_Set Shape: ", train_set_model2.shape)
+# metrics.write_td_evaluation("overworld_train_set_td.txt" ,model1name, "Overworld Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= train_set_model2_reduced, thresh= thresh))
 
 
-# Evaluation AI-Result-set
-print("evaluating AI_Results2")
-val_set_model2 = np.load(val_dir2 + '/overworld_final_1_samples.npy')
-print("AI_Results Shape: ", val_set_model2.shape)
-metrics.write_td_evaluation("overworld_sample_set_td.txt" ,model1name, "Overworld Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= val_set_model2, thresh= thresh))
+# # Evaluation AI-Result-set
+# print("evaluating AI_Results2")
+# val_set_model2 = np.load(val_dir2 + '/overworld_final_1_samples.npy')
+# print("AI_Results Shape: ", val_set_model2.shape)
+# metrics.write_td_evaluation("overworld_sample_set_td.txt" ,model1name, "Overworld Train Set Evaluation:", metrics.evaluate_tonal_distance(data_set1= val_set_model2, thresh= thresh))
